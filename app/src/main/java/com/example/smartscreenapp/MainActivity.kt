50b1661e92b2afa918730b575c2ecc3ababbc2f8
JesseBennett-lab <j.bennett0505@gmail.com>
2026-06-04 14:30:07 +0200

    updated name

--- app/src/main/java/com/example/smartscreenapp/MainActivity.kt
+++ app/src/main/java/com/example/smartscreenapp/MainActivity.kt
@@ -11,6 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //ST10519056
+        //Jesse Bennett
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
